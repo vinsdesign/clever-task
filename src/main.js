5601da84c62f1f5d294edb6250df17439449e048
vinsdesign <kelvinsaputra024@gmail.com>
2025-06-16 21:28:59 +0800

    feat: installation fonts Plus Jakarta Sans

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import './assets/main.css'
+import './assets/index.css'
+import './assets/fonts.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,7 +3,10 @@ import LogoIcons from '@/components/icons/LogoIcon.vue'
 </script>
 
 <template>
-  <div class="bg-[#F9F9F9]">
-    <LogoIcons />
+  <div class="grid grid-cols-2">
+    <section class="bg-[#F9F9F9]">
+      <LogoIcons />
+    </section>
+    <section class="font-jakartaSans-extraBold">Test</section>
   </div>
 </template>
